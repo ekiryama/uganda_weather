8dc0ce0f0a01f42d5393041ceef6830033d8686c
ekiryama <35138463+ekiryama@users.noreply.github.com>
2023-04-19 07:43:46 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -103,7 +103,13 @@ st.write("Start time:", start_time)
 
 
 
+st.header('Line chart Day 9')
 
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
 
 
 
